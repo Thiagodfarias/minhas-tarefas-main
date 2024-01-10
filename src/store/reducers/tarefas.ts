f286a07a6418e1f9621b1746d6009852244376ad
Thiago Monte <thiagodfarias@gmail.com>
2024-01-10 19:30:58 -0400

    salvando

---
+++ src/store/reducers/tarefas.ts
@@ -0,0 +1,17 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import Tarefa from '../../models/Tarefa'
+import * as enums from '../../utils/enums/Tarefa'
+
+const tarefasSlice = createSlice({
+  name: 'tarefas',
+  initialState: {},
+  reducers: {
+    remover: (state, action: PayloadAction<number>) => {
+      state = state.filter((tarefa) => tarefa.id !== action.payload)
+    }
+  }
+})
+
+export const { remover } = tarefasSlice.actions
+
+export default tarefasSlice.reducer
---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENTE = 'urgente',
+  IMPORTANTE = 'importante',
+  NOMRAL = 'normal'
+}
+
+export enum Status {
+  PENDENTE = 'pendente',
+  CONCLUIDA = 'concluída'
+}
